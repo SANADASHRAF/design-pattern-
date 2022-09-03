cfb2d78becc18a631a6f339f8f56e92531d740af
SANADASHRAF <76807340+SANADASHRAF@users.noreply.github.com>
2022-09-03 23:25:11 +0200

    strategy-pattern

--- DesignPatternn/DesignPatternn/Program.cs
+++ DesignPatternn/DesignPatternn/Program.cs
@@ -10,24 +10,188 @@ namespace DesignPatternn
     {
         static void Main(string[] args)
         {
-            #region strategy pattern
-            batta b1 = new batta1();
-            b1.perform();
-            Console.WriteLine("//////////////////////");
-            batta b2 = new batta2();
-            b2.perform();
-            ((batta1)b1).shot();
-            b1.perform();
+
+            #region strategy pattern example 1
+            //batta b1 = new batta1();
+            //b1.perform();
+            //Console.WriteLine("//////////////////////");
+            //batta b2 = new batta2();
+            //b2.perform();
+            //((batta1)b1).shot();
+            //b1.perform();
+            //Console.WriteLine("===========end of strategy patern batta=============");
             #endregion
+
+            #region strategy patern example2
+            //dog d1 = new german();
+            //d1.show();
+            //dog d2 = new potpol();
+            //d2.show();
+            //dog d3 = new wilf();
+            //d3.show();
+            //Console.WriteLine("===========end of strategy patern dog=============");
+            #endregion
+            var observer1 = new HRSpecialist("Bill");
+            var observer2 = new HRSpecialist("John");
+            var provider = new ApplicationsHandler();
+            observer1.Subscribe(provider);
+            observer2.Subscribe(provider);
+            provider.AddApplication(new (1, "Jesus"));
+            provider.AddApplication(new (2, "Dave"));
+            observer1.ListApplications();
+            observer2.ListApplications();
+            observer1.Unsubscribe();
+            Console.WriteLine();
+            Console.WriteLine($"{observer1.Name} unsubscribed");
+            Console.WriteLine();
+
+        }
+
+    }
+    #region observer pattern
+
+    public class Application//observers
+    {
+        public int JobId { get; set; }
+        public string ApplicantName { get; set; }
+
+        public Application(int jobId, string applicantName)
+        {
+            JobId = jobId;
+            ApplicantName = applicantName;
         }
     }
-    #region strategy pattern
+
+    ///////////////////////////////////
+
+    public class HRSpecialist : IObserver<Application>
+    {
+        private IDisposable _cancellation;
+
+        // previous code
+        public string Name { get; set; }
+        public List<Application> Applications { get; set; }
+        public HRSpecialist(string name, List<Application> Applications)
+        {
+            Name = name;
+           this. Applications =Applications ;
+        }
+
+        public void ListApplications()
+        {
+            if (Applications.Any())
+                foreach (var app in Applications)
+                    Console.WriteLine($"Hey, {Name}! {app.ApplicantName} has just applied for job no. {app.JobId}");
+            else
+                Console.WriteLine($"Hey, {Name}! No applications yet.");
+        }
+        //////////////////////
+        public virtual void Subscribe(ApplicationsHandler provider)
+        {
+            _cancellation = provider.Subscribe(this);
+        }
+
+        public virtual void Unsubscribe()
+        {
+            _cancellation.Dispose();
+            Applications.Clear();
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine($"Hey, {Name}! We are not accepting any more applications");
+        }
+
+        public void OnError(Exception error)
+        {
+            // This is called by the provider if any exception is raised, no need to implement it here
+        }
+
+        public void OnNext(Application value)
+        {
+            Applications.Add(value);
+        }
+    }
+
+
+    /// //////////////////////////////
+
+
+
+    /// /////////////////////////
+    public class ApplicationsHandler : IObservable<Application>
+    {
+        private readonly List<IObserver<Application>> _observers;
+        public List<Application> Applications { get; set; }
+
+        public ApplicationsHandler(List<IObserver<Application>> _observers, List<Application> Applications)
+        {
+            this._observers = _observers;
+            this.Applications = Applications;
+        }
+
+        public IDisposable Subscribe(IObserver<Application> observer)
+        {
+            if (!_observers.Contains(observer))
+            {
+                _observers.Add(observer);
+
+                foreach (var item in Applications)
+                    observer.OnNext(item);
+            }
+
+            return new Unsubscriber(_observers, observer);
+        }
+
+        public void AddApplication(Application app)
+        {
+            Applications.Add(app);
+
+            foreach (var observer in _observers)
+                observer.OnNext(app);
+        }
+
+        public void CloseApplications()
+        {
+            foreach (var observer in _observers)
+                observer.OnCompleted();
+
+            _observers.Clear();
+        }
+    }
+
+
+    /// //////////////////////////
+    public class Unsubscriber : IDisposable
+    {
+        private readonly List<IObserver<Application>> _observers;
+        private readonly IObserver<Application> _observer;
+
+        public Unsubscriber(List<IObserver<Application>> observers, IObserver<Application> observer)
+        {
+            _observers = observers;
+            _observer = observer;
+        }
+
+        public void Dispose()
+        {
+            if (_observers.Contains(_observer))
+                _observers.Remove(_observer);
+        }
+    }
+    /////////////////////////////////////////////////
+    
+
+    #endregion
+
+    #region strategy pattern examble 1
     public abstract class batta
     {
         private static int id = 1;
         private string name = "batta";
 
         //member variable
+        //combosition
         protected flybehavior myflybehavior;
 
         public batta()
@@ -52,6 +216,8 @@ namespace DesignPatternn
         }
     }
 
+
+
     public class batta1 : batta
     {
 
@@ -66,6 +232,8 @@ namespace DesignPatternn
         }
     }
 
+
+
     public class batta2 : batta
     {
         public batta2()
@@ -78,6 +246,9 @@ namespace DesignPatternn
         }
 
     }
+
+
+
     public interface flybehavior
     {
         string fly();
@@ -95,6 +266,118 @@ namespace DesignPatternn
         public string fly()
         {
             return "i fly with my wings";
+        }
+    }
+    #endregion
+
+    #region strategy patern example2
+    public abstract class dog
+    {
+       static int id = 1;
+       
+
+        //member variable (acomposion from interface class encapsolate what varus)
+        public ferocitybehaviour force;
+
+       public dog()
+        {
+            Console.WriteLine("i,m " + this.display()+" "+"num "+id );
+            id++;
+            this.force = new strongforocity();
+        }
+
+        public  string sound()
+        {
+            return "my sound is hoho";
+        }
+
+        public string swim()
+        {
+            return "i,m agood swimmer dog";
+        }
+        public abstract string display();
+
+        public void show()
+        {
+            Console.WriteLine(this.force.ferocity());
+            Console.WriteLine(this.sound());
+            Console.WriteLine(this.swim());
+        }
+        
+    }
+
+    //extended class
+     public class german:dog
+    {
+         public  german()
+            {
+            this.force = new weekforocity();
+            }
+
+        public override string display()
+        {
+            return "german";
+        }
+    }
+
+    //extended class
+    public class potpol : dog
+    {
+        public potpol()
+        {
+            this.force = new mediumforocity();
+        }
+
+        public override string display()
+        {
+            return "potpol";
+        }
+    }
+
+    //extended class
+    public class wilf : dog
+    {
+        public wilf()
+        {
+            this.force = new strongforocity();
+        }
+
+        public override string display()
+        {
+            return "wilf";
+        }
+    }
+
+    ///هنا بقى هطبق الcomposition (هستخدم الانترفيس كلاس ك member variable)
+    public interface  ferocitybehaviour
+    {
+        string ferocity();
+    }
+
+    //extended class1
+    public class strongforocity:ferocitybehaviour
+    {
+      public  string ferocity()
+        {
+            return "strong forocity";
+        }
+    }
+
+    //extended class2
+    public class mediumforocity : ferocitybehaviour
+    {
+        public string ferocity()
+        {
+            return "medium forocity";
+        }
+    }
+
+    //extended class3
+    public class weekforocity : ferocitybehaviour
+    {
+        public string ferocity()
+        {
+            return "week forocity";
         }
     }
     #endregion
--- DesignPatternn/DesignPatternn/revision.cs
+++ DesignPatternn/DesignPatternn/revision.cs
@@ -1,12 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+        using System.Text;
 using System.Threading.Tasks;
 
 namespace DesignPatternn
 {
     class revision
     {
+
+        #region abstract class
+        public abstract class abstraction
+        {
+            //contain access modifire
+            //cintain membars
+            //you have all right to override method or not
+            //method can not have implemntation in abstract class
+            //you can not take object from abstract class
+            public int x { set; get; }
+            //لازم تورث وهنا بتكون بدون امبليمنتيشن
+            public abstract int sum(int a, int b);
+            public void print()
+            {
+                Console.WriteLine("hallo");
+            }
+        }
+
+        public class s : abstraction
+        {
+            public override int sum(int a, int b)
+            {
+                return a * b;
+            }
+        }
+      
+        #endregion
+        #region interface class
+        public interface interfacee
+        {
+            //not contain access modifire(as all is public)
+            //not contain membars
+            //you must override method that you right in interface
+            //method can not have implemntation in abstract class(defination only)
+            //you can not take object     
+            //لازم تورث وهنا بتكون بدون امبليمنتيشن
+            int sum(int a, int b);
+
+        }
+
+        class f : interfacee
+        {
+            public int sum(int a, int b)
+            {
+                return a * b;
+            }
+        }
+        #endregion
+
+        #region polymarphism
+        //تعدد الاشكال(one thing have multiple form )
+        // مثلا عندك class have function sound  for animal    
+        // فدلوقتى عندك كلب وقطة وحمار هل كلهم نفس الصوت لا طبعا هنا بقى يجى مفهوم البوليمرفيزم
+        //هيورثو من الكلاس دة وكل واحد هيعمل امبليمنتيشن لفانكشن الصوا دى بطريقة مختلفة حسب استخدامة    
+        //فى نوعين compile time(overloading) && run time polymarphism(overriding)
+        class sound
+        {
+            public virtual void so()
+            {
+                Console.WriteLine("i,m the parent");
+            }
+        }
+
+        class dog : sound
+        {
+            public override void so()
+            {
+                Console.WriteLine("i,m dog");
+            }
+        }
+
+        class cat : sound
+        {
+            public override void so()
+            {
+                Console.WriteLine("i,m cat");
+            }
+        }
+        #endregion
+
+        #region encapsolation(data-hiding.)
+        //he variables or data of a class are hidden from any other class and can be accessed ....
+        //only through any member function of own class in which they are declared.
+        //seter and geter
+        public class enc
+        {
+            private int x { set; get; }
+        }
+        #endregion
     }
 }
